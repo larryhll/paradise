141132e78cf397370852e39a765e350406feb6f9
Liang-li,Huang <liang-li.huang@hpe.com>
2016-12-01 00:57:35 +0800

    level two category item update form issue

--- dist/js/script.js
+++ dist/js/script.js
@@ -38,6 +38,14 @@
 * FUNP - v1.0.0 
 * © Copyright 2016  Liangli Huang
  */
+/*! 
+* FUNP - v1.0.0 
+* © Copyright 2016  Liangli Huang
+ */
+/*! 
+* FUNP - v1.0.0 
+* © Copyright 2016  Liangli Huang
+ */
 /**
  * Created by hualiang on 16-11-20.
  */
